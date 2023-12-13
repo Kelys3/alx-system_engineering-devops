e07e23146f2589a61e7b0c6a51504e845d195758
Kelys3 <fredatd2016@gmail.com>
2023-12-13 05:33:49 +0000

    pycodestyle checked

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,12 +9,12 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Freda Kekeli:0x16-api_advanced/1.0'}
 
     response = requests.get(url, headers=headers).json()
 
     subs_count = response.get("data", {}).get("subscribers", 0)
-    
+
     return subs_count
--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""a recursive function that queries the Reddit API and returns a list containing the
-titles of all hot articles for a given subreddit."""
+"""a recursive function that queries the Reddit API and returns a list
+containing titles of all hot articles for a given subreddit."""
 
 import requests
 
